5617352343628211afd11408d90c5ee88c83e9b8
João <rodriguesjoovtor@gmail.com>
2026-04-11 14:50:15 -0300

    att ambiente de back end configurado para conexão com banco de dados

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,1 +1,28 @@
- 
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker  #permite criar a função assyncrona
+from sqlalchemy.orm import declarative_base   #base declarativa
+from typing import AsyncGenerator #gerador assyncrono
+from dotenv import load_dotenv  #importa da biblioteca dotenv, busca os dados do DB dentro do .env mantendo seguro a aplicação.
+import os 
+
+load_dotenv()
+#Database vai receber a info do .env 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo = True)
+#cria a engine de Async e echo faz com que possamos ver as att do banco
+
+
+#Cria a inicializadora de sessão async
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+#define uma dependencia para colher sessões no FAST API
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
+
+Base = declarative_base()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,11 @@
- 
+from fastapi import FastAPI  #importamos o fast API 
+from app.database import Base, engine, get_async_session   #importamos o que será necessário para inicializar o back end
+
+app = FastAPI() #criamos uma instancia para inicializar
+
+#inicializamos no fast api o evento assim.
+@app.on_event("startup")
+async def startup(): #função de inicializar
+    async with engine.begin() as conn: #conn é connection , aguarda que a engine inicialize a conexão com o banco
+        await conn.run_sync(Base.metadata.create_all) #ele aguarda a conexão para a base criar as tabelas no banco.
+
